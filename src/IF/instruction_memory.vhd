24fcdf3c914e485bcae1d456ae4d55503ef7cd3c
timmfy <khudonogov25@gmail.com>
2024-10-14 15:54:14 +0200

    alu bug

--- src/IF/instruction_memory.vhd
+++ src/IF/instruction_memory.vhd
@@ -16,8 +16,9 @@ architecture behavior of instruction_memory is
         x"01010093", --addi x1, x2, 16
         x"01010113", --addi x2, x2, 16
         x"01008413", --addi x8 x1 16
+        x"01040593", --addi x11, x8, 16
+        x"00710313", --addi x6 x2 8 --the output should be x"0000000000000018" in the register x6
         x"40518233", --sub x4 x3 x5
-        x"00A30293", --addi x5, x6, 10
         x"00A483B3", --add x7 x9 x10
         others => (others => '0')
     );
